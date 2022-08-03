d5d5a0669079c752e7ee91b501ca4a89da888860
Trevor Bos <trevor.bos@outlook.com>
2022-08-03 16:53:11 -0400

    Issue with the naming of variables in .env not matching those in connection.js, seeding the table works with 'npm run seed'

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ const connection = process.env.JAWSDB_URL
   : new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
-      process.env.DB_PASSWORD,
+      process.env.DB_PW,
       {
         host: "localhost",
         dialect: "mysql",
--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,6 @@
-const sequelize = require("../config/config");
-const seedUser = require("./userData");
-const seedPost = require("./postData");
+const sequelize = require("../config/connection");
+const seedUser = require("./userInfo");
+const seedPost = require("./postInfo");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
