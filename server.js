18b8e346acf605d271df06f9c46adfdc3c43964b
Trevor Bos <trevor.bos@outlook.com>
2022-08-03 20:06:57 -0400

    fixed secret again?

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: process.env.secretSession,
+  secret: 'Tech Blog Secret Thing',
   cookie: {},
   resave: false,
   saveUninitialized: true,
